57bacf1ff99d3f123e3fa220920418c1d5fe88b9
Arianof <Arian.fathi03@gmai.com>
2023-01-07 21:28:50 +0330

    feat: elements and export functions imported

---
+++ components/App.js
@@ -0,0 +1,6 @@
+import { showBoard } from "./ShowBoard.js";
+import { insertValues } from "./Solver.js";
+import { solve } from "./Solver.js";
+import { insertAnswers } from "./Solver.js";
+const solveBtn = document.getElementById('solve_btn');
+const resetBtn = document.getElementById('reset_btn');
