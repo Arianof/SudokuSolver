ef88e0704df582616bd5d6b838aa4e73eb6ab860
Arianof <Arian.fathi03@gmai.com>
2023-01-07 21:17:15 +0330

    feat: an condition has added to seprate 3x3 squares

--- components/ShowBoard.js
+++ components/ShowBoard.js
@@ -7,7 +7,13 @@ export function showBoard(){
            inputElement.setAttribute('max' , '9');
            inputElement.setAttribute('min' , '1');
            inputElement.setAttribute('id' , 'cell');
-        
+           if(((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+           ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+           ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) || 
+           ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && i > 27 && i < 53) ||
+           ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)){
+            inputElement.classList.add('odd_section');
+           }
            board.appendChild(inputElement);
        }
 
