10f2968b4d16a6f13f420212c1675249ff53bf1c
Arianof <Arian.fathi03@gmai.com>
2023-01-07 21:30:32 +0330

    feat: main function which run all functions in order added

--- components/App.js
+++ components/App.js
@@ -4,3 +4,20 @@ import { solve } from "./Solver.js";
 import { insertAnswers } from "./Solver.js";
 const solveBtn = document.getElementById('solve_btn');
 const resetBtn = document.getElementById('reset_btn');
+
+function main(){
+ showBoard();
+ solveBtn.addEventListener('click' , () =>{
+  insertValues();
+  if(solve(0 , 0)){
+    insertAnswers();
+  }else{
+   alert("Can't Solve this Sudoku!");
+  }
+ })
+ resetBtn.addEventListener('click' , () =>{
+  window.location.reload(true);
+ })
+
+}
+main();
